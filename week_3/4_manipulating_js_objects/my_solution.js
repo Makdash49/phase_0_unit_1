fdfd37ce9585a315e709ebc9e80e5c98e2fe2370
Mark <markdmcq@gmail.com>
2014-05-30 16:09:10 -0700

    Cleaned it up a little

--- week_3/4_manipulating_js_objects/my_solution.js
+++ week_3/4_manipulating_js_objects/my_solution.js
@@ -17,44 +17,46 @@ var terah = {
 // Write your code below.
 
 
-
+// Create object Adam, with properties of name: "Adam" and spouse: terah.
 var adam = {
   name: "Adam",
-  spouse: terah
+  spouse: terah,
 }
 
+// Give terah the spouse property of adam.
 terah.spouse = adam
+
+//Change terah property of weight to 125.
 terah.weight = 125
+
+//remove terah property eyeColor.
 delete terah.eyeColor
 
-terah.children = []
+
+//create a terah property with it's own variables.
+terah.children = {}
+
+//Add variables of the names and assign them as the children of terah.
+var carson = {name: "Carson"}
+terah.children.carson = carson
+var carter = {name: "Carter"}
+terah.children.carter = carter
+var colton = {name: "Colton"}
+terah.children.colton = colton
 
 
-
-// var adam = {name: "Adam"}
-// terah.spouse = adam
-// terah.weight = 125
-// delete terah["eyeColor"]
-// adam.spouse = terah
-// var carson = {name: "Carson"}
-// var carter = {name: "Carter"}
-// var colton = {name: "Colton"}
-
-// terah.children = []
-// terah.children.carson = carson
-// terah.children.carter = carter
-// terah.children.colton = colton
-
-// adam.children = terah.children
-
+//Set Adam's children equal to Terah's.
+adam.children = terah.children
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines
+// Ok.  I've learned a lot this week.  This was all new to me and although there is much that I do not understand I have come a long way.
+// Fortunately, I got a lot of help from peer paring and I was able to learn a lot of syntax by doing web research.
+// At first, I tried writing a lot of code but then I discovered that little of it worked.  So then I started writing just enough
+// code to run each test, and I kept trying till I got it right.
 // 
-// 
-// 
-// 
+// This week was the first time I have used node and Javascripts so it was good to see how the workflow goes of coding and testing.
 // // 
 // http://stackoverflow.com/questions/208105/how-to-remove-a-property-from-a-javascript-object
 // http://msdn.microsoft.com/en-us/library/ie/kb6te8d3(v=vs.94).aspx
