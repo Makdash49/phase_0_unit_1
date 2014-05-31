8468191f66e63791fce98ada2593c57b1584ff93
Mark <markdmcq@gmail.com>
2014-05-30 19:06:55 -0700

    CC-Mission

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,12 +1,27 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
+// To defeat the Orge you gather your troops and move towards the Ogre.
+//Once the Ogre sees you he will start to come after you.
+//Run away and have your troops follow.  Go to the the boat on the side of the river.
+//Get in and your troops will get in too.  Then the boat will start to go down the river towards the waterfall.
+//Tell your troops to row across the river.  Tell them to "Row faster!"
+//At this point the Ogre will have gotten into his own boat and will be coming down the river.
+//He will not be able to row as fast as your troops so he will end up going over the waterfall to his death.
+//You and your troops will land safely on the other side of the river.
 // 
 // 
 
 // Pseudocode
-// 
+// Say "follow me" to troops
+//Move to middle of screen
+//Taunt Ogre
+//Ogre wil begin to follow
+//Run away!
+//Get in boat and troops will follow.
+//Boat will start to go down river.
+//Message to troops: "Start rowing!"
+//Second message to troops "Row harder!""
 // 
 // 
 // 
@@ -16,11 +31,30 @@
 
 
 
+this.say.message = "Follow me!"
+
+this.move.XY(52,60);
+
+var taunt=function(taunt) {}
+
+this.move.XY(32,70);
+
+this.say.message = "Get in the boat!"
+
+var getInBoat = function(command) {}
+
+this.say.message = "Start rowing!"
+
+var startRowing = function(command) {}
+
+this.say.message = "Row harder!"
+
+var rowHarder = function(command) {}
 
 
 
 // Refactored Code
-
+//I'm not really sure how to refactor this because I'm not sure how to do it in the first place.
 
 
 
